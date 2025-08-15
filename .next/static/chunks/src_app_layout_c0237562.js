de45a5ead47f8347b9f337561e9c5f03004f2749
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2025-08-15 18:46:46 +0530

    Add .next/static/chunks/src_app_layout_c0237562.js 1755263805926

---
+++ .next/static/chunks/src_app_layout_c0237562.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__85eb511c._.css",
+  "static/chunks/node_modules_d4065aff._.js",
+  "static/chunks/src_160aaf39._.js"
+],
+    source: "dynamic"
+});
