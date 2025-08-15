1c86241d70b2db7ce7554ef475a8ea6ba2676970
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2025-08-15 18:43:57 +0530

    Add next.config.js 1755263637272

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
