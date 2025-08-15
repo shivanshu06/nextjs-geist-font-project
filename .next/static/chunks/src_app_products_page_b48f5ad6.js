ef1d522379c39613048200e60cbee975946dbac5
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2025-08-15 18:45:13 +0530

    Add .next/static/chunks/src_app_products_page_b48f5ad6.js 1755263712434

---
+++ .next/static/chunks/src_app_products_page_b48f5ad6.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_36a119a9._.js",
+  "static/chunks/src_98195337._.js"
+],
+    source: "dynamic"
+});
